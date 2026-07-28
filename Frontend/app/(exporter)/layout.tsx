014c144665f8af2ca62c9c371fd934e7d5734c7b
Abidi-Rayen <abidirayen7805@gmail.com>
2026-07-28 20:51:54 +0100

    fix loading state + Navbar links

--- Frontend/app/(exporter)/layout.tsx
+++ Frontend/app/(exporter)/layout.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import { useCompany } from "@/hooks/useCompany";
-
+import { useRouter } from "next/navigation";
+import Spinner from "@/components/ui/spinner";
 export default function ExporterLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const { isLoading, isError } = useCompany();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+ const router = useRouter();
+ if (isLoading) {
+     return (
+       <div className="flex min-h-screen items-center justify-center bg-olive-950">
+         <Spinner size="h-10 w-10" />
+       </div>
+     );
+   }
 
   if (isError) {
-    return <div>Failed to load company.</div>;
+    router.replace("/login")
   }
 
   return children;
--- Frontend/app/(exporter)/registration/page.tsx
+++ Frontend/app/(exporter)/registration/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+
 import { useRouter } from "next/navigation";
 import { Leaf, CheckCircle2, FileText, RefreshCcw, Plus } from "lucide-react";
 import { useMyRegistrationRequests } from "@/hooks/useMyRegistrationRequests";
